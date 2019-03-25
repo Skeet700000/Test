c0bda231d967aad257233fa38482c3f8b0fbba9b
Som <48624451+Skeet700000@users.noreply.github.com>
2019-03-25 14:09:39 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -7,9 +7,6 @@ import get_pictures
 login, password = "89581779173","700000pam2"
 vk_session = vk_api.VkApi(token="b1d6094d3c8cd58620946a28eafe67429c84ecc58ad8e9dc9a447396f67f039001aedab6c83be993c6886")
 
-# token = "ЗдесьЕстьВашТокен"
-# vk_session = vk_api.VkApi(token=token)
-
 session_api = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
 
@@ -28,3 +25,11 @@ while True:
                 elif response == "котики":
                     attachment = get_pictures.get(vk_session,-130670107,session_api)
                     vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Держи котиков!', 'random_id': 0, "attachment": attachment})
+                elif response == "3":
+                    vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Пока, друг!', 'random_id': 0})
+                elif response == "4":
+                    vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Пока, друг!', 'random_id': 0})
+                elif response == "5":
+                    vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Пока, друг!', 'random_id': 0})
+                elif response == "6":
+                    vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Пока, друг!', 'random_id': 0})
